357d54dbc8399ed5ad9ba26d7c80e53a7fcd2258
dumch <arturdumchev@yandex.ru>
2023-09-30 13:01:38 +0300

    Experimenting with the GHA

--- compile.sh
+++ compile.sh
@@ -14,8 +14,8 @@ native-image -jar dartclojure.jar --no-fallback \
     -J-Dclojure.spec.skip.macros=true -J-Dclojure.compiler.direct-linking=true \
     --verbose --no-server -J-Xmx3G \
     --report-unsupported-elements-at-runtime --native-image-info \
-    # -H:+StaticExecutableWithDynamicLibC \
     -H:CCompilerOption=-pipe \
+    --features=clj_easy.graal_build_time.InitClojureClasses \
     -H:ReflectionConfigurationFiles=META-INF/native-image/logging.json
 
 chmod +x dartclojure
